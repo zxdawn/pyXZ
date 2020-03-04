37381fd0fcb9b1cc60079b8f2bd8d34ae8d7b246
Xin Zhang <xinzhang1215@gmail.com>
2020-03-04 21:59:53 +0800

    Init sonde :balloon:

---
+++ XZ_sonde/__init__.py
@@ -0,0 +1,13 @@
+"""
+XZ_sonde
+
+	Modules and functions for dealing with sonde data:
+		Ozonesonde and Radiosonde
+
+Created by Xin Zhang (c) 2020
+"""
+
+__author__ = "Xin Zhang"
+__email__= "xinzhang1215@gmail.com"
+__date__ = "Feb 16, 2020"
+__url__ = "https://dreambooker.site/"
