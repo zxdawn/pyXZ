545f216fc71c3b9bd2120dd0b227435ebf6f4009
Xin Zhang <xinzhang1215@gmail.com>
2020-03-13 20:35:28 +0800

    Mapping MEIC to WRF-Chem :factory:

---
+++ XZ_model/MEIC/README.md
@@ -0,0 +1,54 @@
+# MEIC
+
+Mapping MEIC nc files to WRF-Chem chemistry (MOZCART)
+
+## Structure
+
+- src
+
+  - mozart.py
+
+    Main script of data processing.
+
+  - plot_wrfchemi.py
+
+    Plot the quickview of generated wrfchemi* file.
+
+  - conversion_table.csv
+
+    Table of converting MEIC species to WRF-Chem species.
+
+  - hourly_factor.csv (Optional)
+
+    Houly factors to distribute hourly emissions.
+
+- input_files
+
+  - geo_em.d\<n\>.nc
+
+  - ├── \<yyyy\>
+
+    ├     ├── CB05
+
+    ├     ├── ├── \<yyyy\>_\<mm\>\_\_agriculture\_\_CB05*.nc
+
+    ├     ├── RADM2
+
+    ├     ├── ├── \<yyyy\>_\<mm\>\_\_agriculture\_\_RAMD2*.nc
+
+    ├     ├── SAPRC07
+
+    ├     ├── ├── \<yyyy\>_\<mm\>\_\_agriculture\_\_SAPRC07*.nc
+
+    ├     ├── SAPRC99
+
+    ├     ├── ├── \<yyyy\>_\<mm\>\_\_agriculture\_\_SAPRC99*.nc
+
+- output files
+
+  - wrfchemi\_00z\_d<domain>
+  - wrfchemi\_12z\_d<domain>
+
+## Example
+
+<img src="https://github.com/zxdawn/pyXZ/raw/master/XZ_model/MEIC/emission_example.jpg" width="400">
---
+++ XZ_model/MEIC/src/plot_wrfchemi.py
@@ -0,0 +1,44 @@
+'''
+INPUT:
+    wrfchemi_d<domain>
+
+OUTPUT:
+    Figures of emission distribution
+
+UPDATE:
+    Xin Zhang:
+       03/13/2020: Basic
+'''
+
+import proplot as plot
+import xarray as xr
+from datetime import datetime
+
+def npbytes_to_str(var):
+    return (bytes(var).decode("utf-8"))
+
+# read data
+ds = xr.open_dataset('../output_files/wrfchemi_00z_d01')
+# take 06 UTC and 24 MOZCART species as an example
+t = 6
+# get info of time
+Times = ds['Times']
+time_str = datetime.strptime("".join(npbytes_to_str(Times[t].values)),
+                                "%Y-%m-%d_%H:%M:%S").strftime("%Y-%m-%d_%H:%M:%S")
+# get keys except "Times"
+species = ds.drop_vars('Times')
+
+# plot
+f, axs = plot.subplots(tight=True, share=0,
+                        nrows=6, ncols=4,
+                       )
+
+axs.format(suptitle=time_str+' nearest')
+for index, key in enumerate(species):
+    m = axs[index].pcolormesh(ds[key][t, ...], cmap='Fire')
+    axs[index].format(title=ds[key].attrs['description'])
+    axs[index].colorbar(m, loc='r',
+               label=ds[key].attrs['units'],
+               tickminor=False)
+
+f.savefig('emission_example.png')
