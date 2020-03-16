27d910dd26130948ba9658f21e87b2278b46c433
Xin Zhang <xinzhang1215@gmail.com>
2020-03-16 21:59:27 +0800

    Support generating area from wrfout :globe_with_meridians:

--- XZ_model/wrfchem.py
+++ XZ_model/wrfchem.py
@@ -11,6 +11,12 @@ from netCDF4 import Dataset
 from wrf import getvar, latlon_coords, ALL_TIMES
 from pyresample.geometry import AreaDefinition, SwathDefinition
 
+wrf_projs = {1: 'lcc',
+             2: 'npstere',
+             3: 'merc',
+             6: 'eqc'
+             }
+
 class read_wps(object):
     def __init__(self, wps_path, domain):
         self.get_info(wps_path, domain)
@@ -19,32 +25,24 @@ class read_wps(object):
         # read basic info from geo file generrated by WPS
         self.geo = xr.open_dataset(wps_path + 'geo_em.'+domain+'.nc')
         attrs = self.geo.attrs
-        # read original attrs
-        self.lat_1  = attrs['TRUELAT1']
-        self.lat_2  = attrs['TRUELAT2']
-        self.lat_0  = attrs['MOAD_CEN_LAT']
-        self.lon_0  = attrs['STAND_LON']
-        self.cenlat = attrs['CEN_LAT']
-        self.cenlon = attrs['CEN_LON']
-        self.i      = attrs['i_parent_end']
-        self.j      = attrs['j_parent_end']
-        self.dx     = attrs['DX']
-        self.dy     = attrs['DY']
-        self.mminlu = attrs['MMINLU']
-        self.map    = attrs['MAP_PROJ']
-        self.eta    = attrs['BOTTOM-TOP_GRID_DIMENSION']
+        i     = attrs['i_parent_end']
+        j     = attrs['j_parent_end']
 
         # calculate attrs for area definition
-        shape = (self.j, self.i)
-        center = (0, 0)
-        radius = (self.i*self.dx/2, self.j*self.dy/2)
+        shape = (j, i)
+        radius = (i*attrs['DX']/2, j*attrs['DY']/2)
 
         # create area as same as WRF
         area_id = 'wrf_circle'
-        proj_dict = {'proj': 'lcc', 'lat_0': self.lat_0, 'lon_0': self.lon_0, \
-                    'lat_1': self.lat_1, 'lat_2': self.lat_2, \
-                    'a':6370000, 'b':6370000}
-        self.area = AreaDefinition.from_circle(area_id, proj_dict, center, radius, shape=shape)
+        proj_dict = {'proj': wrf_projs[attrs['MAP_PROJ']],
+                     'lat_0': attrs['MOAD_CEN_LAT'],
+                     'lon_0': attrs['STAND_LON'],
+                     'lat_1': attrs['TRUELAT1'],
+                     'lat_2': attrs['TRUELAT2'],
+                     'a':6370000,
+                     'b':6370000}
+        center = (0, 0)
+        self.area_def = AreaDefinition.from_circle(area_id, proj_dict, center, radius, shape=shape)
 
 class read_wrf(object):
     def __init__(self, wrf_path, fname, vnames=None):
@@ -67,3 +65,24 @@ class read_wrf(object):
                 if v != 'Times' and 'lon' not in self.dv:
                     self.dv.update({'lat' : latlon_coords(self.dv[v])[0]})
                     self.dv.update({'lon': latlon_coords(self.dv[v])[1]})
+
+        # get proj
+        attrs = xr.open_dataset(wrf_path+fname).attrs
+        i     = attrs['WEST-EAST_GRID_DIMENSION']
+        j     = attrs['SOUTH-NORTH_GRID_DIMENSION']
+
+        # calculate attrs for area definition
+        shape = (j, i)
+        radius = (i*attrs['DX']/2, j*attrs['DY']/2)
+
+        # create area as same as WRF
+        area_id = 'wrf_circle'
+        proj_dict = {'proj': wrf_projs[attrs['MAP_PROJ']],
+                     'lat_0': attrs['MOAD_CEN_LAT'],
+                     'lon_0': attrs['STAND_LON'],
+                     'lat_1': attrs['TRUELAT1'],
+                     'lat_2': attrs['TRUELAT2'],
+                     'a':6370000,
+                     'b':6370000}
+        center = (0, 0)
+        self.area_def = AreaDefinition.from_circle(area_id, proj_dict, center, radius, shape=shape)
