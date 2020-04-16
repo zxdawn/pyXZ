352a7cab12566f48384eb663bde824e188e75b4d
Xin Zhang <xinzhang1215@gmail.com>
2020-04-16 22:09:08 +0800

    Update script for chuan

--- XZ_flight/flight_altitude.py
+++ XZ_flight/flight_altitude.py
@@ -27,26 +27,26 @@ file_1 = './data/0512.xlsx'
 file_2 = './data/1227.xlsx'
 sheet_name = 'Sheet1'
 
-plane  = './data/plane.jpg'
-google_map = './data/google_map.png'
+plane = './data/plane.jpg'
+google_map = './data/google_map.jpg'
 
-save_dir = './'
+save_dir = './figures/'
 outputname = 'IN_flight.png'
 
 def add_map(ax, map_data,
-              west, east,
-              south, north,
-              lon_d, lat_d):
+            west, east,
+            south, north,
+            lon_d, lat_d):
     ax.add_feature(map_data, edgecolor='k', linewidth=.2)
     add_grid(ax, ccrs.PlateCarree(),
-              west, east,
-              south, north,
-              lon_d, lat_d,
-              xlabel_size=10, ylabel_size=10,
-              grid_color='grey')
+             west, east,
+             south, north,
+             lon_d, lat_d,
+             xlabel_size=12, ylabel_size=12,
+             grid_color='grey')
 
 def read_file(file, sheet_name):
-    f   = pd.read_excel(file, sheet_name=sheet_name)
+    f = pd.read_excel(file, sheet_name=sheet_name)
     # t   = f['CreationTime'].values
     lon = f['Long (deg)'].values
     lat = f['Lat (deg)'].values
@@ -66,7 +66,9 @@ def multicolored_lines(ax, lon, lat, alt, cmap, linewidth, alpha):
     norm = plt.Normalize(vmin=500, vmax=5000)
     lc = colorline(ax, lon, lat, alt, norm, cmap, linewidth, alpha)
     cbar = plt.colorbar(lc)
-    cbar.set_label('Altitude (m)')
+    cbar.ax.tick_params(labelsize=12)
+    cbar.set_label('Altitude (m)', fontsize=12)
+    cbar.ax.yaxis.set_label_position('left')
 
 def colorline(ax, lon, lat, alt, norm, cmap, linewidth, alpha):
     """
@@ -97,7 +99,7 @@ def make_segments(x, y):
 def main():
     fig = plt.figure(figsize=[12, 12])
     projection = ccrs.PlateCarree()
-    provinces  = load_province(projection)
+    provinces = load_province(projection)
     city = load_city(projection)
     county = load_china_county_level_city(projection)
 
@@ -106,15 +108,27 @@ def main():
     im = image.imread(plane)
     ax.imshow(im)
     ax.axis('off')
-    ax.annotate("(a)", xy=(0.02, 0.93), xycoords="axes fraction")
+    ax.annotate("(a)", xy=(0.02, 0.93), xycoords="axes fraction", fontsize=15)
     ax.set_anchor('W')
-    
+
     # ----- subplot2 ----- #
-    ax = fig.add_subplot(222)
-    im = image.imread(google_map)
-    ax.imshow(im)
-    ax.axis('off')
-    ax.annotate("(b)", xy=(0.02, 0.93), xycoords="axes fraction")
+    # ax = fig.add_subplot(222)
+    # im = image.imread(google_map)
+    # ax.imshow(im)
+    # ax.axis('off')
+    # ax.annotate("(b)", xy=(0.02, 0.93), xycoords="axes fraction", fontsize=15)
+    # ax.set_anchor('W')
+
+    ax = fig.add_subplot(222, projection=projection)
+    south = 35; north = 40; west = 112; east = 118
+    lon_d = 1; lat_d = 0.5
+    add_map(ax, provinces, west, east, south, north, lon_d, lat_d)
+
+    ax.plot(114.5, 38.02, 'r*', markersize=10)
+    ax.text(114.6, 38.02, 'SJZ', color='red', fontsize=15, fontweight='bold')
+    ax.plot(114.38, 37.2, 'r*', markersize=10)
+    ax.text(114.5, 37.2, 'XT', color='red', fontsize=15, fontweight='bold')
+    ax.annotate("(b)", xy=(0.02, 0.93), xycoords="axes fraction", fontsize=15)
     ax.set_anchor('W')
 
     # ----- subplot3 ----- #
@@ -122,14 +136,14 @@ def main():
     file = file_1
     sheet_name = 'Sheet1'
     lon, lat, alt, south, north, west, east = read_file(file, sheet_name)
-    south = 37.95; north=38.4; west=114.4; east=114.85
+    south = 37.95; north = 38.4; west = 114.4; east = 114.85
     lon_d = 0.1; lat_d = 0.1
     add_map(ax, county, west, east, south, north, lon_d, lat_d)
 
     im = multicolored_lines(ax, lon, lat, alt, cmap='viridis', linewidth=1, alpha=1)
     ax.plot(114.5, 38.02, 'r*', markersize=10)
     ax.text(114.6, 38.02, 'SJZ', color='red', fontsize=15, fontweight='bold')
-    ax.annotate("(c)", xy=(0.02, 0.93), xycoords="axes fraction")
+    ax.annotate("(c)", xy=(0.02, 0.93), xycoords="axes fraction", fontsize=15)
     ax.set_anchor('W')
 
     # ----- subplot4 ----- #
@@ -137,14 +151,14 @@ def main():
     file = file_2
     sheet_name = 'Sheet1'
     lon, lat, alt, south, north, west, east = read_file(file, sheet_name)
-    south = 36.5; north=38.5; west=113.5; east=115.5
+    south = 36.5; north = 38.5; west = 113.5; east = 115.5
     lon_d = 0.5; lat_d = 0.5
     add_map(ax, city, west, east, south, north, lon_d, lat_d)
 
     im = multicolored_lines(ax, lon, lat, alt, cmap='viridis', linewidth=1, alpha=1)
     ax.plot(114.38, 37.2, 'r*', markersize=5)
     ax.text(114.5, 37.2, 'XT', color='red', fontsize=15, fontweight='bold')
-    ax.annotate("(d)", xy=(0.02, 0.93), xycoords="axes fraction")
+    ax.annotate("(d)", xy=(0.02, 0.93), xycoords="axes fraction", fontsize=15)
     ax.set_anchor('W')
 
     plt.show()
