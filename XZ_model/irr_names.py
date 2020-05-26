ecd2acf99610ab914f025246b7a9ac08bbb6863e
Xin Zhang <xinzhang1215@gmail.com>
2020-05-26 20:13:02 +0800

    Bugfix: check whether the species is substring

--- XZ_model/irr_names.py
+++ XZ_model/irr_names.py
@@ -33,6 +33,18 @@ def read_eqn(dir, eqn_file, species):
                 reaction = re.split('} | :', line)[1]
                 # delete space and tab
                 reaction = re.sub(r'\s+', '', reaction)
+                # get the content after "="
+                right = reaction.split('=')[1]
+                # get the index of species
+                species_idx = right.index(species)
+                # check whether there's number before the species
+                if species_idx != 0 and right[species_idx-1] != '+':
+                    # if the character two digits before the species is letter
+                    # then it is actually another species
+                    # which just contains the substring of species
+                    if species_idx-1 != 0 and right[species_idx-2].isalpha():
+                            continue
+
                 # only the reactants are right ...
                 # I don't decide to fix it now,
                 # as I only need the reactants to generate the IRR varname
